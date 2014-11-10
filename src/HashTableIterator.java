9b3bc8bf7e8ec2b6dcf382fd13939ff215c5b204
melindar <melindar@rams.colostate.edu>
2014-11-09 23:06:17 -0700

    Added HashTableIterator stub.

---
+++ src/HashTableIterator.java
@@ -0,0 +1,35 @@
+// PA4
+// Authors: David Thorpe, Melinda Ryan 
+// Date: 11/19/2014
+// Class: CS200
+
+import java.util.Iterator;
+
+
+public class HashTableIterator implements Iterator<Term>
+{
+	private HashTable hashTable;
+	
+	public HashTableIterator(HashTable hashTable) {
+
+		this.hashTable = hashTable; 
+	}
+	public boolean hasNext() 
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Term next()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void remove() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
